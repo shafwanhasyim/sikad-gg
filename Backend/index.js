906600e9c0dd791abdef23c85dcf6fec783d7ca6
shafwanhasyim <shafwan178@gmail.com>
2025-06-08 23:48:05 +0700

    change cors be

--- Backend/index.js
+++ Backend/index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 4000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: process.env.FRONTEND_URL,
   credentials: true,
 }));
 
